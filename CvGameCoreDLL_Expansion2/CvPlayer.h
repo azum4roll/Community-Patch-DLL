8b3812c9fdf75f63f4039621a3edc2681f116848
axatin <95587882+axatin@users.noreply.github.com>
2026-05-17 11:31:20 +0200

    (10-120) Subsidies Free Coal

--- CvGameCoreDLL_Expansion2/CvPlayer.h
+++ CvGameCoreDLL_Expansion2/CvPlayer.h
@@ -2076,7 +2076,9 @@ public:
 
 	int getResourceFromCSAlliances(ResourceTypes eIndex) const;
 	void changeResourceFromCSAlliances(ResourceTypes eIndex, int iChange);
-	void setResourceFromCSAlliances(ResourceTypes eIndex, int iChange);
+
+	int getFreeResourceFromPolicies(ResourceTypes eIndex) const;
+	void changeFreeResourceFromPolicies(ResourceTypes eIndex, int iChange);
 
 	const std::vector<ResourceTypes>& GetResourcesNotForSale() const { return m_vResourcesNotForSale; }
 	bool IsResourceNotForSale(ResourceTypes eResource);
@@ -3421,6 +3423,7 @@ protected:
 	std::vector<int> m_paiJFDPoliticPercent;
 	std::vector<int> m_aiYieldFromMinors;
 	std::vector<int> m_paiResourceFromCSAlliances;
+	std::vector<int> m_paiFreeResourceFromPolicies;
 	std::vector<int> m_paiResourceShortageValue;
 	std::vector<int> m_aiYieldFromBirth;
 	std::vector<int> m_aiYieldFromBirthCapital;
@@ -4207,6 +4210,7 @@ SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldRateModifier)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_paiJFDPoliticPercent)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromMinors)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_paiResourceFromCSAlliances)
+SYNC_ARCHIVE_VAR(std::vector<int>, m_paiFreeResourceFromPolicies)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_paiResourceShortageValue)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromBirth)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromBirthCapital)
