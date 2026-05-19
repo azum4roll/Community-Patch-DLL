589668718b90af2572b737385b828ac281fcc7ee
azum4roll <azum4roll@users.noreply.github.com>
2026-05-19 17:31:05 +0800

    (10-048b) Penitentiary, No BGP On Tiles Version https://forums.civfanatics.com/threads/10-048b-penitentiary-no-bgp-on-tiles-version.702978/ fixed security per pop text rewrote constabulary and police station strategy texts (were vanilla)

--- (2) Vox Populi/Database Changes/AI/TechFlavorSweeps.sql
+++ (2) Vox Populi/Database Changes/AI/TechFlavorSweeps.sql
@@ -263,10 +263,12 @@ VALUES
 	('TECH_STEAM_POWER', 'FLAVOR_WONDER', 10),
 	('TECH_STEAM_POWER', 'FLAVOR_MILITARY_TRAINING', 5),
 
-	('TECH_RIFLING', 'FLAVOR_DEFENSE', 10), -- Units: Fusilier, Field Gun, ForeignLegion, Wonders: ChateauDIf, Yields: Camp +1G
+	('TECH_RIFLING', 'FLAVOR_DEFENSE', 10), -- Units: Fusilier, Field Gun, ForeignLegion, Buildings: Penitentiary, Wonders: ChateauDIf, Yields: Camp +1G
 	('TECH_RIFLING', 'FLAVOR_OFFENSE', 20),
 	('TECH_RIFLING', 'FLAVOR_RANGED', 10),
 	('TECH_RIFLING', 'FLAVOR_TILE_IMPROVEMENT', 5),
+	('TECH_RIFLING', 'FLAVOR_WONDER', 10),
+	('TECH_RIFLING', 'FLAVOR_ESPIONAGE', 15),
 
 	-------------------------------------------------------
 
